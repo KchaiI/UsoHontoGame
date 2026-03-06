dbf047ab9a39958bc9dbabe84490a199200b0e1d
kaichi0michael <ihciak.0127@gmail.com>
2026-03-06 16:06:18 +0900

    画面遷移の実装

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -7,6 +7,7 @@
 
 'use client';
 
+import Link from 'next/link';
 import { LanguageSwitcher } from './LanguageSwitcher';
 
 /**
@@ -18,7 +19,7 @@ export function Header() {
   return (
     <header className="border-b border-gray-200 bg-white">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3">
-        <div className="text-lg font-semibold text-gray-900">ウソホントゲーム</div>
+        
         <LanguageSwitcher />
       </div>
     </header>
